570a285560ad24ed624a460f742ec237e05a98b3
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-02 22:30:46 +0530

    Dev config

--- configs/clean.js
+++ configs/clean.js
@@ -1,3 +1,19 @@
-const fs = require("fs");
+const fs = require('fs');
 
-fs.rmSync("app/dist", { recursive: true, force: true });
+const clean = ['node_modules', 'app/dist', 'release/build', 'package-lock.json'];
+
+switch (process.argv[2]) {
+  case '--clean':
+    clean.forEach((dir) => {
+      fs.rmSync(dir, { recursive: true, force: true });
+    });
+    break;
+  case '--dist':
+    fs.rmSync('app/dist', { recursive: true, force: true });
+    break;
+  case '--build':
+    fs.rmSync('release/build', { recursive: true, force: true });
+    break;
+  default:
+    console.log('Empty action received');
+}
