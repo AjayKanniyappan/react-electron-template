552d7901460ba9200cdc7caa00671b797bdb026a
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-04 16:00:37 +0530

    Webpack React Configs

--- configs/webpack.main.prod.ts
+++ configs/webpack.main.prod.ts
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import webpack from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const Configuration: webpack.Configuration = {
   mode: 'production',
   devtool: 'source-map',
   target: 'electron-main',
+  stats: 'errors-only',
   entry: {
     main: path.join(__dirname, '../src/main/main.ts'),
     preload: path.join(__dirname, '../src/main/preload.ts'),
@@ -19,27 +21,32 @@ const Configuration: webpack.Configuration = {
         use: {
           loader: 'ts-loader',
           options: {
-            // Remove this line to enable type checking in webpack builds
             transpileOnly: true,
+            compilerOptions: {
+              module: 'esnext',
+            },
           },
         },
       },
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        ELECTRON_ENV: JSON.stringify(process.env.ELECTRON_ENV || 'production'),
-      },
+    new webpack.EnvironmentPlugin({
+      ELECTRON_ENV: JSON.stringify(process.env.ELECTRON_ENV || 'production'),
     }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [path.join(__dirname, '../src'), 'node_modules'],
     plugins: [
       new TsconfigPathsPlugin({
         configFile: path.resolve(__dirname, '../tsconfig.json'),
       }),
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   output: {
     path: path.join(__dirname, '../app/dist/main'),
---
+++ configs/webpack.renderer.prod.ts
@@ -0,0 +1,119 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+import webpack from 'webpack';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const Configuration: webpack.Configuration = {
+  mode: 'production',
+  devtool: 'source-map',
+  target: 'electron-renderer',
+  stats: 'errors-only',
+  entry: path.join(__dirname, '../src/renderer/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            compilerOptions: {
+              module: 'esnext',
+            },
+          },
+        },
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          'sass-loader',
+        ],
+        include: /\.module\.s?(c|a)ss$/,
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /\.module\.s?(c|a)ss$/,
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      /* SVG LOADER */
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, '../src/renderer/index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+      },
+      isBrowser: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new webpack.EnvironmentPlugin({
+      ELECTRON_ENV: JSON.stringify(process.env.ELECTRON_ENV || 'production'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [path.join(__dirname, '../src'), 'node_modules'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  output: {
+    path: path.join(__dirname, '../app/dist/renderer'),
+    filename: '[name].js',
+    publicPath: './',
+  },
+};
+
+export default Configuration;
